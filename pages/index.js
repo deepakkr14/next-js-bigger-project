3b5a1eb114cf9b65931b346d0685a24e455c8fdb
deepakkr14 <deepakbipin142@gmail.com>
2024-05-12 17:58:02 +0530

    added useEffect and useState

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,42 @@
-import Meetuplist from  '../components/meetups/MeetupList'
+import { useEffect ,useState} from "react";
+import Meetuplist from "../components/meetups/MeetupList";
 const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A First Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some address 5, 12345 some city",
-      description: "This is a Fist meetup!",
-    },
-    {
-      id: "m2",
-      title: "A Second Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some address 10, 12345 some city",
-      description: "This is a Second meetup!",
-    },
-  ];
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 some city",
+    description: "This is a Fist meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 some city",
+    description: "This is a Second meetup!",
+  },
+];
 const Homepage = () => {
+  const [Metupdata,setMetupdata]=useState([]);
+  useEffect(()=>{
+    //some api fetch going on...
+    setMetupdata(DUMMY_MEETUPS)
+  })
   return (
     <div>
       <h1>i am home page</h1>
-    <Meetuplist meetups={DUMMY_MEETUPS}/> 
+      <Meetuplist meetups={Metupdata} />
     </div>
-  )
+  );
+};
+export async function getStaticProps() {
+  // fetchin the data
+  return {
+    props: { meetups: DUMMY_MEETUPS },
+    revalidate:10
+  };
 }
 
 export default Homepage;
